acbe53e2e008c0e590973ee6e9572f042badde98
xyaelx <be.yael.10@gmail.com>
2020-06-14 15:15:49 +0300

    fixed bug with val data loader

--- network.py
+++ network.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 import numpy as np
 import torchvision
 import matplotlib.pyplot as plt
@@ -69,20 +65,15 @@ data_transforms = {
 }
 
 # # Data Loader
-
-
-
-
 BATCH_SIZE = 1
 ''' The function takes the data loader and a parameter  '''
-
 
 def create_train_val_slice(image_datasets, sample_size=None, val_same_as_train=False):
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
     if not sample_size:  # return the whole data
         dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=BATCH_SIZE,
-                                                      shuffle=True, num_workers=1)
+                                                      shuffle=True, num_workers=0)
                        for x in ['train', 'val']}
         return dataloaders, dataset_sizes
 
@@ -90,12 +81,12 @@ def create_train_val_slice(image_datasets, sample_size=None, val_same_as_train=F
                 for x in ['train', 'val']}
 
     image_datasets_reduced = {
-        x: torch.utils.data.Subset(image_datasets['train' if val_same_as_train else x], sample_n['train'])
+        x: torch.utils.data.Subset(image_datasets['train' if val_same_as_train else x], sample_n[x])
         for x in ['train', 'val']}
     dataset_sizes = {x: len(image_datasets_reduced[x]) for x in ['train', 'val']}
 
     dataloaders_reduced = {x: torch.utils.data.DataLoader(image_datasets_reduced[x], batch_size=BATCH_SIZE,
-                                                          shuffle=True, num_workers=1) for x in ['train', 'val']}
+                                                          shuffle=True, num_workers=0) for x in ['train', 'val']}
     return dataloaders_reduced, dataset_sizes
 
 
@@ -107,7 +98,7 @@ class_names = image_datasets['train'].classes
 
 # sample_size = 100
 # data, dataset_sizes =  create_train_val_slice(image_datasets,sample_size,True)
-data, dataset_sizes = create_train_val_slice(image_datasets, sample_size=5)
+my_data, dataset_sizes = create_train_val_slice(image_datasets, sample_size=5)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Classes: ", class_names)
@@ -156,10 +147,10 @@ def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2, chec
         best_acc = checkpoint['best_val_accuracy']
 
     print("Starting epochs")
-    outer = tqdm(total=num_epochs, desc='Epoch', position=0)
-    inner = tqdm(total=(dataset_sizes['train'] // BATCH_SIZE), position=1)
+    # outer = tqdm(total=num_epochs, desc='Epoch', position=0)
+    # inner = tqdm(total=(dataset_sizes['train'] // BATCH_SIZE), position=1)
     for epoch in range(num_epochs):
-        outer.update(1)
+        # outer.update(1)
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
@@ -191,7 +182,7 @@ def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2, chec
                     report_str = f'[{epoch + 1}, {i}] loss: {(running_loss / i * inputs.size(0)):.3f}'
                 else:
                     report_str = "first iteration"
-
+                print(report_str)
                 # Update inner tqdm, we are about to override the previous maximum, update maximum
                 # inner.update(1)  # Advance the tqdm counter
                 # inner.desc = f'Phase: {phase} ' + report_str
@@ -231,7 +222,7 @@ def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2, chec
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
                 # save the weights
-                torch.save(best_model_wts, CHECK_POINT_PATH)
+                # torch.save(best_model_wts, CHECK_POINT_PATH)
         print()
 
     time_elapsed = time.time() - since
@@ -243,94 +234,73 @@ def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2, chec
     model.load_state_dict(best_model_wts)
     return model, best_loss, best_acc
 
-model_conv = torchvision.models.resnet18(pretrained=True)
-# model_conv = torchvision.models.resnet50(pretrained=True)
-# model_conv = torchvision.models.resnet101(pretrained=True)
+
+def main():
+    global model_conv
+    model_conv = torchvision.models.resnet18(pretrained=True)
+    # model_conv = torchvision.models.resnet50(pretrained=True)
+    # model_conv = torchvision.models.resnet101(pretrained=True)
+    # model_conv.eval()
+    # # Train Model
+    # Parameters of newly constructed modules have requires_grad=True by default
+    ct = 0
+    for child in model_conv.children():
+        ct += 1
+        # freezes layers 1-6 in the total 10 layers of Resnet50
+        if ct < 7:
+            for param in child.parameters():
+                param.requires_grad = False
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, len(class_names))
+    model_conv = model_conv.to(device)
+    '''two options to write the loss. They are both equal'''
+    # option 1 #
+    criterion = nn.CrossEntropyLoss()
+    # option 2 #
+    # p = nn.functional.softmax(model_conv, dim=1)
+    # # to calculate loss using probabilities you can do below
+    # criterion = nn.functional.nll_loss(torch.log(p), y)
+    # Observe that only parameters of final layer are being optimized
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=70, gamma=0.1)
+    # CHECK_POINT_PATH = 'ModelParams' / 'checkpoint.tar'
+    # !del $CHECK_POINT_PATH
+    # try:
+    #     checkpoint = torch.load(CHECK_POINT_PATH)
+    #     print("checkpoint loaded")
+    # except:
+    #     checkpoint = None
+    #     print("checkpoint not found")
+    model_conv, best_val_loss, best_val_acc = train_model(my_data,
+                                                          model_conv,
+                                                          criterion,
+                                                          optimizer_conv,
+                                                          exp_lr_scheduler,
+                                                          num_epochs=5,
+                                                          checkpoint=None)
+    # torch.save({'model_state_dict': model_conv.state_dict(),
+    #             'optimizer_state_dict': optimizer_conv.state_dict(),
+    #             'best_val_loss': best_val_loss,
+    #             'best_val_accuracy': best_val_acc,
+    #             'scheduler_state_dict': exp_lr_scheduler.state_dict(),
+    #             }, CHECK_POINT_PATH)
+    # # Test Model
+    model_conv.eval()
+    from pprint import pprint
+    x = 'train'
+    d = datasets.ImageFolder(os.path.join(DATA_DIR, x))
+    cnt = Counter([])
+    for i, (image, category) in enumerate(d):
+        cnt.update({(image_datasets['train'].classes)[category]: 1})
+    print(cnt)
+    # In[17]:
+    image_datasets['train'].classes[0]
+    # In[ ]:
 
 
-# model_conv.eval()
-
-
-# # Train Model
-
-
-# Parameters of newly constructed modules have requires_grad=True by default
-
-ct = 0
-for child in model_conv.children():
-    ct += 1
-    # freezes layers 1-6 in the total 10 layers of Resnet50
-    if ct < 7:
-        for param in child.parameters():
-            param.requires_grad = False
-
-num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, len(class_names))
-model_conv = model_conv.to(device)
-
-'''two options to write the loss. They are both equal'''
-# option 1 #
-criterion = nn.CrossEntropyLoss()
-
-# option 2 #
-# p = nn.functional.softmax(model_conv, dim=1)
-# # to calculate loss using probabilities you can do below
-# criterion = nn.functional.nll_loss(torch.log(p), y)
-
-# Observe that only parameters of final layer are being optimized
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=70, gamma=0.1)
-
-
-# CHECK_POINT_PATH = 'ModelParams' / 'checkpoint.tar'
-
-# !del $CHECK_POINT_PATH
-
-try:
-    checkpoint = torch.load(CHECK_POINT_PATH)
-    print("checkpoint loaded")
-except:
-    checkpoint = None
-    print("checkpoint not found")
-
-
-model_conv, best_val_loss, best_val_acc = train_model(data,
-                                                      model_conv,
-                                                      criterion,
-                                                      optimizer_conv,
-                                                      exp_lr_scheduler,
-                                                      num_epochs = 5,
-                                                      checkpoint = checkpoint)
-
-torch.save({'model_state_dict': model_conv.state_dict(),
-            'optimizer_state_dict': optimizer_conv.state_dict(),
-            'best_val_loss': best_val_loss,
-            'best_val_accuracy': best_val_acc,
-            'scheduler_state_dict': exp_lr_scheduler.state_dict(),
-            }, CHECK_POINT_PATH)
-
-# # Test Model
-
-model_conv.eval()
-
-
-from pprint import pprint
-
-x = 'train'
-d = datasets.ImageFolder(os.path.join(DATA_DIR, x))
-cnt = Counter([])
-for i, (image, category) in enumerate(d):
-    cnt.update({(image_datasets['train'].classes)[category]: 1})
-print(cnt)
-
-# In[17]:
-
-
-image_datasets['train'].classes[0]
-
-# In[ ]:
-
+if __name__ == '__main__':
+    main()
 
 
 
