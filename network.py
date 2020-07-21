dd7f87f979c1031b49c59a4eeebd3ff72051ee38
xYaelx <be.yael.10@gmail.com>
2020-07-21 12:16:28 +0300

    unfreeze all layers = train all layers

--- network.py
+++ network.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy as np
 import torchvision
 import matplotlib.pyplot as plt
@@ -13,6 +15,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, models, transforms
 # import itertools
 # import pixiedust
@@ -27,7 +30,6 @@ print(torch.__version__)
 plt.ion()  # interactive mode
 torch.cuda.is_available()
 
-
 # try:
 #     from google.colab import drive
 #
@@ -37,7 +39,6 @@ torch.cuda.is_available()
 #     LABS_DIR = Path('C:/Labs/')
 
 DATA_DIR = 'Data'
-
 
 #### sanity check for the images
 # classes = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
@@ -71,6 +72,7 @@ data_transforms = {
 ### Data Loader ###
 ''' The function takes the data loader and a parameter  '''
 
+
 def create_train_val_slice(image_datasets, sample_size=None, val_same_as_train=False):
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
@@ -89,7 +91,7 @@ def create_train_val_slice(image_datasets, sample_size=None, val_same_as_train=F
     # clone the image_datasets_reduced[train] for the val
     if val_same_as_train:
         image_datasets_reduced['val'] = list(image_datasets_reduced['train'])
-        image_datasets_reduced['train'] = image_datasets_reduced['val'] #copy the train to val (so the tranformations won't occur again)
+        image_datasets_reduced['train'] = image_datasets_reduced['val']  # copy the train to val (so the tranformations won't occur again)
 
     dataset_sizes = {x: len(image_datasets_reduced[x]) for x in ['train', 'val']}
 
@@ -133,7 +135,7 @@ print(f'Validation image size: {dataset_sizes["val"]}')
 # print(f"classes={classes}")
 # imshow(sample_train_images, title=[class_names[i] for i in classes])
 
-def freeze_layers_grad(model, total_freeze_layers = 7):
+def freeze_layers_grad(model, total_freeze_layers=7):
     # Parameters of newly constructed modules have requires_grad=True by default
     layer = 0
     for child in model.children():
@@ -143,20 +145,16 @@ def freeze_layers_grad(model, total_freeze_layers = 7):
             for param in child.parameters():
                 param.requires_grad = False
 
-def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2):
+def train_model(data, model, label_criterion, optimizer, scheduler, num_epochs=2, writer=None):
     since = time.time()
 
-    train_accuracy_history = []
-    train_loss_history = []
-
-    test_accuracy_history = []
-    test_loss_history = []
-
     print("Starting epochs")
-    for epoch in range(num_epochs):
-        print(f'Epoch: {epoch + 1} of {num_epochs}')
+    for epoch in range(1, num_epochs + 1):
+        print(f'Epoch: {epoch} of {num_epochs}')
         model.train()  # Set model to training mode
         running_test_loss = 0.0
+        running_corrects = 0.0
+
         for i, (inputs, labels) in enumerate(data['train']):
             # data['train'] contains (input,labels) for every batch (so i=[1...NUM OF BATCHES]
             inputs = inputs.to(device)
@@ -169,28 +167,103 @@ def train_model(data, model, criterion, optimizer, scheduler, num_epochs=2):
             with torch.set_grad_enabled(True):
                 outputs = model(inputs)
                 _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
-                train_accuracy_history.append(torch.sum(preds == labels.data)/inputs.size(0))
-                train_loss_history.append(loss/inputs.size(0))
-                print(f'Train Loss: {loss/inputs.size(0):.4f} Train Acc: {torch.sum(preds == labels.data)/inputs.size(0):.4f}')
+                label_loss = label_criterion(outputs, labels)
 
                 # backward + optimize only if in training phase
                 loss.backward()
                 optimizer.step()
 
-        scheduler.step() #scheduler step is performed per-epoch
+            batch_loss = label_loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
 
-        epoch_loss, epoch_acc = eval_model(criterion, data, model, optimizer)
+            if writer is not None:  # save train label_loss for each batch
+                x_axis = 1000 * (epoch + i / (dataset_sizes['train'] // BATCH_SIZE))
+                writer.add_scalar('batch label_loss', batch_loss / BATCH_SIZE, x_axis)
 
-        # save epoch loss and accuracy
-        test_accuracy_history.append(epoch_acc)
-        test_loss_history.append(epoch_loss)
+        if scheduler is not None:
+            scheduler.step()  # scheduler step is performed per-epoch in the training phase
 
+        train_acc = running_corrects / dataset_sizes['train']
+        if writer is not None:  # save epoch accuracy
+            x_axis = epoch
+            writer.add_scalar('accuracy-train',
+                              train_acc,
+                              x_axis)
+
+        epoch_loss, epoch_acc = eval_model(label_criterion, data, model, optimizer)
+
+        if writer is not None:  # save epoch accuracy
+            x_axis = epoch
+            writer.add_scalar('accuracy-val',
+                              epoch_acc,
+                              x_axis)
+
+    # TODO check stop condition by overfit
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    #return the last trained model
-    return model, test_accuracy_history, test_loss_history
+    # return the last trained model
+    return model
+
+def train_model(data, model, label_criterion, optimizer, scheduler, num_epochs=2, writer=None):
+    since = time.time()
+
+    print("Starting epochs")
+    for epoch in range(1, num_epochs + 1):
+        print(f'Epoch: {epoch} of {num_epochs}')
+        model.train()  # Set model to training mode
+        running_test_loss = 0.0
+        running_corrects = 0.0
+
+        for i, (inputs, labels) in enumerate(data['train']):
+            # data['train'] contains (input,labels) for every batch (so i=[1...NUM OF BATCHES]
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            with torch.set_grad_enabled(True):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                label_loss = label_criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                label_loss.backward()
+                optimizer.step()
+
+            batch_loss = label_loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+            if writer is not None:  # save train label_loss for each batch
+                x_axis = 1000 * (epoch + i / (dataset_sizes['train'] // BATCH_SIZE))
+                writer.add_scalar('batch label_loss', batch_loss / BATCH_SIZE, x_axis)
+
+        if scheduler is not None:
+            scheduler.step()  # scheduler step is performed per-epoch in the training phase
+
+        train_acc = running_corrects / dataset_sizes['train']
+        if writer is not None:  # save epoch accuracy
+            x_axis = epoch
+            writer.add_scalar('accuracy-train',
+                              train_acc,
+                              x_axis)
+
+        epoch_loss, epoch_acc = eval_model(label_criterion, data, model, optimizer)
+
+        if writer is not None:  # save epoch accuracy
+            x_axis = epoch
+            writer.add_scalar('accuracy-val',
+                              epoch_acc,
+                              x_axis)
+
+    # TODO check stop condition by overfit
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    # return the last trained model
+    return model
 
 
 def eval_model(criterion, data, model, optimizer):
@@ -215,7 +288,7 @@ def eval_model(criterion, data, model, optimizer):
             loss = criterion(outputs, labels)
 
         # statistics - sum loss and accuracy on all batches
-        running_loss += loss.item() * inputs.size(0) #item.loss() is the average loss of the batch
+        running_loss += loss.item() * inputs.size(0)  # item.loss() is the average loss of the batch
         running_corrects += torch.sum(preds == labels.data)
 
     epoch_loss = running_loss / dataset_sizes['val']
@@ -223,58 +296,71 @@ def eval_model(criterion, data, model, optimizer):
     print(f'Test Loss: {epoch_loss:.4f} TestAcc: {epoch_acc:.4f}')
     return epoch_loss, epoch_acc
 
-### Test Model ###
-def test_model():
-    global model_conv
+
+def get_model():
     model_conv = torchvision.models.resnet18(pretrained=True)
     # model_conv = torchvision.models.resnet50(pretrained=True)
     # model_conv = torchvision.models.resnet101(pretrained=True)
-    # model_conv.eval()
-    ### Train Model ###
-    freeze_layers_grad(model_conv)
+
     num_ftrs = model_conv.fc.in_features
-    model_conv.fc = nn.Linear(num_ftrs, len(class_names))
+    model_conv.fc = nn.Sequential()
+    classifier = nn.Linear(num_ftrs, len(class_names))
     model_conv = model_conv.to(device)
-    '''two options to write the loss. They are both equal'''
-    # option 1 #
-    criterion = nn.CrossEntropyLoss()
-    # option 2 #
-    # p = nn.functional.softmax(model_conv, dim=1)
-    # # to calculate loss using probabilities you can do below
-    # criterion = nn.functional.nll_loss(torch.log(p), y)
-    # Observe that only parameters of final layer are being optimized
-    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
-    # Decay LR by a factor of 0.1 every 7 epochs
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
-
-    model_conv, test_accuracy_history, test_loss_history = train_model(my_data,
-                                                          model_conv,
-                                                          criterion,
-                                                          optimizer_conv,
-                                                          exp_lr_scheduler,
-                                                          num_epochs=NUM_EPOCHS)
-
-    # torch.save({'model_state_dict': model_conv.state_dict(),
-    #             'optimizer_state_dict': optimizer_conv.state_dict(),
-    #             'best_val_loss': best_val_loss,
-    #             'best_val_accuracy': best_val_acc,
-    #             'scheduler_state_dict': exp_lr_scheduler.state_dict(),
-    #             }, CHECK_POINT_PATH)
-    # model_conv.eval()
-
-    # x = 'train'
-    # d = datasets.ImageFolder(os.path.join(DATA_DIR, x))
-    # cnt = Counter([])
-    # for i, (image, category) in enumerate(d):
-    #     cnt.update({(image_datasets['train'].classes)[category]: 1})
-    # print(cnt)
-    # image_datasets['train'].classes[0]
+    return model_conv
 
 
+# Tensorboard Stuff
+def run_experiment(data, lr_initial, gamma, step_size, weight_decay, num_of_epochs):
+    """
+    Gets all hyper parameters and creates the relevant optimizer and scheduler according to those params
 
+    """
+    model_net = get_model()
+    criterion = nn.CrossEntropyLoss()  # softmax+log
+    optimizer = optim.Adam(model_net.parameters(), lr=lr_initial, weight_decay=weight_decay)
+
+    # decay LR by a factor of gamma every step_size epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    # check optimizer and schedular due to ADAM
+
+    experiment_name = datetime.now().strftime("%Y%m%d-%H%M%S") + f'_lr_{lr_initial}_st_{step_size}_gma_{gamma}_wDK_{weight_decay}'
+    print("Experiment name: ", experiment_name)
+
+    writer = SummaryWriter('runs/' + experiment_name)
+    trained_model = train_model(data,
+                                model_net,
+                                criterion,
+                                optimizer,
+                                exp_lr_scheduler,
+                                num_epochs=num_of_epochs,
+                                writer=writer)
+    return trained_model
+
+
+def main():
+    for lr in [0.0005, 0.0001]:
+        for scheduler_step_size in [5, 7, 9]:
+            for scheduler_gamma in [0.1, 0.3, 0.5]:
+                for weight_decay in [0.01, 0.1]:
+                    model_conv = run_experiment(my_data, lr, scheduler_gamma, scheduler_step_size, weight_decay,
+                                                NUM_EPOCHS)
+
+        # torch.save({'model_state_dict': model_conv.state_dict(),
+        #             'optimizer_state_dict': optimizer.state_dict(),
+        #             'best_val_loss': best_val_loss,
+        #             'best_val_accuracy': best_val_acc,
+        #             'scheduler_state_dict': exp_lr_scheduler.state_dict(),
+        #             }, CHECK_POINT_PATH)
+        # model_conv.eval()
+
+        # x = 'train'
+        # d = datasets.ImageFolder(os.path.join(DATA_DIR, x))
+        # cnt = Counter([])
+        # for i, (image, category) in enumerate(d):
+        #     cnt.update({(image_datasets['train'].classes)[category]: 1})
+        # print(cnt)
+        # image_datasets['train'].classes[0]
 
 if __name__ == '__main__':
-    test_model()
-
-
+    main()
 
