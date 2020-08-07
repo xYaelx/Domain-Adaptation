6fd79bd571a1d674479bc2ce3c958c6a06ceb8e7
xYaelx <be.yael.10@gmail.com>
2020-08-07 18:37:00 +0300

    changed file by the added dataLoader class

--- network.py
+++ network.py
@@ -3,6 +3,9 @@ from datetime import datetime
 import numpy as np
 import torchvision
 import matplotlib.pyplot as plt
+from torch.utils.data import dataloader
+
+import DataLoaders
 import time
 import os
 from pathlib import Path
@@ -40,8 +43,8 @@ torch.cuda.is_available()
 #     LABS_DIR = Path('C:/Labs/')
 
 class TrainingParams:
-    def __init__(self, lr, weight_decay, step_size, gamma, num_epochs):
-        self.model = get_model()
+    def __init__(self, lr, weight_decay, step_size, gamma, num_epochs,num_classes):
+        self.model = get_model(num_classes)
         self.label_criterion = nn.CrossEntropyLoss()  # softmax+log
         self.domain_criterion = nn.binary_cross_entropy_with_logits # TODO check this loss criterion
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
@@ -56,61 +59,8 @@ class TrainingParams:
 #     !ls $DATA_DIR\VAL\$emotion | wc -l
 
 
-# Data augmentation and normalization for training
-# for validatin we use normalization and resize (for train we also change the angle and size of the images)
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomRotation(5),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop(224, scale=(0.96, 1.0), ratio=(0.95, 1.05)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize([224, 224]),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'test': transforms.Compose([
-        transforms.Resize([224, 224]),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-}
-
-
-### Data Loader ###
-def create_train_val_slice(image_datasets, sample_size=None, val_same_as_train=False):
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-
-    if not sample_size:  # return the whole data
-        dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=BATCH_SIZE,
-                                                      shuffle=True, num_workers=0)
-                       for x in ['train', 'val']}
-        return dataloaders, dataset_sizes
-
-    sample_n = {x: random.sample(list(range(dataset_sizes[x])), sample_size)
-                for x in ['train', 'val']}
-
-    image_datasets_reduced = {x: torch.utils.data.Subset(image_datasets[x], sample_n[x])
-                              for x in ['train', 'val']}
-
-    # clone the image_datasets_reduced[train] for the val
-    if val_same_as_train:
-        image_datasets_reduced['val'] = list(image_datasets_reduced['train'])
-        # copy the train to val (so the tranformations won't occur again)
-        image_datasets_reduced['train'] = image_datasets_reduced['val']
-
-    dataset_sizes = {x: len(image_datasets_reduced[x]) for x in ['train', 'val']}
-
-    dataloaders_reduced = {x: torch.utils.data.DataLoader(image_datasets_reduced[x], batch_size=BATCH_SIZE,
-                                                          shuffle=True, num_workers=0) for x in ['train', 'val']}
-    return dataloaders_reduced, dataset_sizes
-
-
 # image_datasets = {x: datasets.ImageFolder(os.path.join(DATA_DIR, x), data_transforms[x])
 #                   for x in ['train', 'val']}
-# class_names = image_datasets['train'].classes
 # my_data, dataset_sizes = create_train_val_slice(image_datasets, sample_size=SAMPLE_SIZE)
 
 
@@ -139,7 +89,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # imshow(sample_train_images, title=[class_names[i] for i in classes])
 
 
-def train_model(males_data, females_data, training_params, writer=None):
+def train_model(males_dataloader, females_dataloader, training_params, writer=None):
     since = time.time()
 
     print("Starting epochs")
@@ -147,7 +97,7 @@ def train_model(males_data, females_data, training_params, writer=None):
         print(f'Epoch: {epoch} of {training_params.num_epochs}')
         training_params.model.train()  # Set model to training mode
         running_corrects = 0.0
-        join_dataloader = zip(males_data['train'], females_data['train'])  # TODO check how females_data is built
+        join_dataloader = zip(males_dataloader.data['train'], females_dataloader.data['train'])  # TODO check how females_data is built
         for i, ((males_x, males_label), (females_x, _)) in enumerate(join_dataloader):
             # data['train'] contains (males_x, males_y) for every batch (so i=[1...NUM OF BATCHES]
             samples = torch.cat([males_x, females_x])
@@ -165,7 +115,7 @@ def train_model(males_data, females_data, training_params, writer=None):
                 label_loss = training_params.label_criterion(label_preds, label_y)
 
                 # TODO check the discriminator
-                extracted_features = training_params.model.activation['avgpool']  # Size: torch.Size([16, 512, 1, 1])
+                extracted_features = training_params.model.avgpool.activation['avgpool']  # Size: torch.Size([16, 512, 1, 1])
                 extracted_features = extracted_features.view(extracted_features.shape[0], -1)
                 domain_preds = training_params.model.discriminator(extracted_features).squeeze()
                 domain_loss = training_params.domain_criterion(domain_preds, domain_y)
@@ -179,15 +129,15 @@ def train_model(males_data, females_data, training_params, writer=None):
             running_corrects += torch.sum(label_preds.max(1)[1] == label_y.data)
 
             if writer is not None:  # save train label_loss for each batch
-                x_axis = 1000 * (epoch + i / (dataset_sizes['train'] // BATCH_SIZE))
+                x_axis = 1000 * (epoch + i / (males_dataloader.dataset_size['train'] // BATCH_SIZE))#TODO devidie by batch size or batch size//2?
                 writer.add_scalar('batch label_loss', batch_loss / BATCH_SIZE, x_axis)
 
         if training_params.scheduler is not None:
             training_params.scheduler.step()  # scheduler step is performed per-epoch in the training phase
 
-        train_acc = running_corrects / dataset_sizes['train'] # TODO change the accuracy ratio by the relevant dataset
+        train_acc = running_corrects / males_dataloader.dataset_size['train'] # TODO change the accuracy ratio by the relevant dataset
 
-        epoch_loss, epoch_acc = eval_model(males_data, training_params)
+        epoch_loss, epoch_acc = eval_model(males_dataloader, training_params)
 
         if writer is not None:  # save epoch accuracy
             x_axis = epoch
@@ -200,12 +150,12 @@ def train_model(males_data, females_data, training_params, writer=None):
     # return the last trained model
     return training_params.model
 
-def eval_model(data, training_params):
+def eval_model(dataloader, training_params):
     training_params.model.eval()  # Set model to evaluate mode
     running_loss = 0.0
     running_corrects = 0
 
-    for i, (inputs, labels) in enumerate(data['val']):
+    for i, (inputs, labels) in enumerate(dataloader.data['val']):
         # data['val'] contains (input,labels) for every batch (so i=[1...NUM OF BATCHES]
 
         inputs = inputs.to(device)
@@ -225,13 +175,13 @@ def eval_model(data, training_params):
         running_loss += loss.item() * inputs.size(0)  # item.loss() is the average loss of the batch
         running_corrects += torch.sum(outputs.max(1)[1] == labels.data)
 
-    epoch_loss = running_loss / dataset_sizes['val']
-    epoch_acc = running_corrects.double() / dataset_sizes['val']
+    epoch_loss = running_loss / dataloader.dataset_size['val'] #TODO change to male dataset size
+    epoch_acc = running_corrects.double() / dataloader.dataset_size['val'] #TODO change to male dataset size
     print(f'Test Loss: {epoch_loss:.4f} TestAcc: {epoch_acc:.4f}')
     return epoch_loss, epoch_acc
 
 
-def get_model():
+def get_model(class_names):
     model_conv = torchvision.models.resnet18(pretrained=True)
     # model_conv = torchvision.models.resnet50(pretrained=True)
     # model_conv = torchvision.models.resnet101(pretrained=True)
@@ -278,26 +228,21 @@ def run_experiment(data_m, data_f, lr_initial, gamma, step_size, weight_decay, n
 
 
 def main():
-    image_m_dataset = {x: datasets.ImageFolder(os.path.join(DATA_DIR_M, x), data_transforms[x])
-                       for x in ['train', 'val']}
-    data_male, dataset_male_sizes = create_train_val_slice(image_m_dataset, sample_size=SAMPLE_SIZE,
-                                                           val_same_as_train=False)
+    dataloder_male = DataLoaders(sample_size=SAMPLE_SIZE, batch_size=BATCH_SIZE // 2, data_dir=DATA_DIR_M,
+                                 val_same_as_train=False)
+    dataloder_female = DataLoaders(sample_size=SAMPLE_SIZE, batch_size=BATCH_SIZE // 2, data_dir=DATA_DIR_F,
+                                   val_same_as_train=False)
 
-    image_f_dataset = {x: datasets.ImageFolder(os.path.join(DATA_DIR_F, x), data_transforms[x])
-                       for x in ['train', 'val']}
-    data_female, dataset_female_sizes = create_train_val_slice(image_f_dataset, sample_size=SAMPLE_SIZE,
-                                                               val_same_as_train=False)
-    print("Classes: ", class_names)
-    print(f'Train image size: {dataset_sizes["train"]}')
-    print(f'Validation image size: {dataset_sizes["val"]}')
+    print("Classes: ", dataloder_male.classes)
+    print(f'Train image size: {dataloder_male.dataset_sizes["train"]}')
+    print(f'Validation image size: {dataloder_male.dataset_sizes["val"]}')
 
-    class_names = image_m_dataset['train'].classes
     for lr in [0.0005, 0.0001]:
         for scheduler_step_size in [5, 7, 9]:
             for scheduler_gamma in [0.1, 0.3, 0.5]:
                 for weight_decay in [0.01, 0.1]:
-                    model_conv = run_experiment(data_male, data_female, lr, scheduler_gamma, scheduler_step_size, weight_decay,
-                                                NUM_EPOCHS)
+                    model_conv = run_experiment(dataloder_male, dataloder_female, lr, scheduler_gamma,
+                                                scheduler_step_size, weight_decay, NUM_EPOCHS)
 
         # torch.save({'model_state_dict': model_conv.state_dict(),
         #             'optimizer_state_dict': optimizer.state_dict(),
